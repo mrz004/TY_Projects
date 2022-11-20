a14ac9c471cd23755da4295d6c5a3b157f335f9c
mrz <mrz@localhost.localdomain>
2022-11-20 18:53:26 +0530

    initial commit

---
+++ CarManager.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.sql.*;
+
+public class CarManager extends JFrame {
+    CarManager() {
+        this.setSize(800, 600);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new CarManager();
+    }
+}
